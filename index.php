cfedb8b6ea3408adcbc1db91d7c4aec919958bb6
hamid <hamidyoqoub@mail.utoronto.ca>
2016-02-25 02:34:37 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Toogle</title>
+    <link rel='stylesheet' type='text/css' href='style.css'>
+    <link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <h1>Please login or Register</h1>
+    <a href='login_screen.php'>Login<a/>
+    <a href='register_screen.php'>Register<a/>
+</body>
+
+</html>
